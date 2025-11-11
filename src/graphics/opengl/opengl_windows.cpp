faa41814fa944456f91b4ade521356f94d75c1e3
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-11-11 04:00:26 +0200

    moving kw gl runtime content to kalagraphics

---
+++ src/graphics/opengl/opengl_windows.cpp
@@ -0,0 +1,181 @@
+//Copyright(C) 2025 Lost Empire Entertainment
+//This program comes with ABSOLUTELY NO WARRANTY.
+//This is free software, and you are welcome to redistribute it under certain conditions.
+//Read LICENSE.md for more information.
+
+#ifdef _WIN32
+
+#include "graphics/opengl/opengl_functions_core.hpp"
+#include "graphics/opengl/opengl_functions_win.hpp"
+
+#include "KalaHeaders/log_utils.hpp"
+
+#include "core/core.hpp"
+#include "graphics/opengl/opengl.hpp"
+
+using KalaHeaders::Log;
+using KalaHeaders::LogType;
+
+using KalaGraphics::Core::KalaGraphicsCore;
+using namespace KalaGraphics::Graphics::OpenGLFunctions;
+
+using std::to_string;
+
+namespace KalaGraphics::OpenGL
+{
+	void OpenGL_Core::SwapOpenGLBuffers(u32 windowID)
+	{
+		if (windowContexts.empty()
+			|| !windowContexts.contains(windowID))
+		{
+			Log::Print(
+				"Cannot swap OpenGL buffers for window '" + to_string(windowID) + "' because the window doesn't exist!",
+				"OPENGL",
+				LogType::LOG_ERROR,
+				2);
+
+			return;
+		}
+		
+		HDC storedHDC{};
+		if (windowContexts[windowID].hdc == NULL)
+		{
+			Log::Print(
+				"Cannot swap OpenGL buffers for window '" + to_string(windowID) + "' because its hdc is not assigned!",
+				"OPENGL",
+				LogType::LOG_ERROR,
+				2);
+
+			return;
+		}
+
+		storedHDC = ToVar<HDC>(windowContexts[windowID].hdc);
+		SwapBuffers(storedHDC);
+	}
+	
+	void OpenGL_Core::MakeContextCurrent(u32 windowID)
+	{
+		if (windowContexts.empty()
+			|| !windowContexts.contains(windowID))
+		{
+			Log::Print(
+				"Cannot make OpenGL context current for window '" + to_string(windowID) + "' because the window doesn't exist!",
+				"OPENGL",
+				LogType::LOG_ERROR,
+				2);
+
+			return;
+		}
+		
+		HDC storedHDC{};
+		HGLRC storedHGLRC{};
+		if (windowContexts[windowID].hdc == NULL
+			|| windowContexts[windowID].hglrc == NULL)
+		{
+			Log::Print(
+				"Cannot make OpenGL context current for window '" + to_string(windowID) + "' because its hdc or hglrc is not assigned!",
+				"OPENGL",
+				LogType::LOG_ERROR,
+				2);
+
+			return;
+		}
+
+		storedHDC = ToVar<HDC>(windowContexts[windowID].hdc);
+		storedHGLRC = ToVar<HGLRC>(windowContexts[windowID].hglrc);
+
+		if (wglGetCurrentContext() != storedHGLRC) wglMakeCurrent(storedHDC, storedHGLRC);
+	}
+	bool OpenGL_Core::IsContextValid(u32 windowID)
+	{
+		if (windowContexts.empty()
+			|| !windowContexts.contains(windowID))
+		{
+			Log::Print(
+				"Cannot check OpenGL context validity for window '" + to_string(windowID) + "' because the window doesn't exist!",
+				"OPENGL",
+				LogType::LOG_ERROR,
+				2);
+
+			return false;
+		}
+		
+		HGLRC storedHGLRC{};
+		if (windowContexts[windowID].hglrc == NULL)
+		{
+			Log::Print(
+				"Cannot check OpenGL context validity for window '" + to_string(windowID) + "' because its hglrc is not assigned!",
+				"OPENGL",
+				LogType::LOG_ERROR,
+				2);
+
+			return false;
+		}
+
+		storedHGLRC = ToVar<HGLRC>(windowContexts[windowID].hglrc);
+
+		HGLRC current = wglGetCurrentContext();
+		if (!current)
+		{
+			Log::Print(
+				"Cannot check OpenGL context validity for window '" + to_string(windowID) + "' because the context is null!",
+				"OPENGL",
+				LogType::LOG_ERROR,
+				2);
+
+			return false;
+		}
+
+		if (current != storedHGLRC)
+		{
+			KalaGraphicsCore::ForceClose(
+				"OpenGL error",
+				"Current OpenGL context does not match stored context!");
+
+			return false;
+		}
+
+		return true;
+	}
+	
+	void OpenGL_Core::SetOpenGLLibrary()
+	{
+		HMODULE handle = GetModuleHandleA("opengl32.dll");
+		if (!handle)
+		{
+			KalaGraphicsCore::ForceClose(
+				"OpenGL error",
+				"Failed to get handle for 'opengl32.dll'");
+
+			return;
+		}
+
+		openGL32Lib = FromVar(handle);
+	}
+	
+	void OpenGL_Core::SetVSyncState(
+		u32 windowID,
+		VSyncState newValue)
+	{
+		if (windowContexts.contains(windowID)) return;
+		
+		if (!wglSwapIntervalEXT)
+		{
+			Log::Print(
+				"wglSwapIntervalEXT not supported! VSync setting ignored.",
+				"OPENGL",
+				LogType::LOG_ERROR,
+				2);
+				
+			return;
+		}
+		
+		windowContexts[windowID].vsyncState = newValue;
+
+		wglSwapIntervalEXT(newValue == VSyncState::VSYNC_ON
+			? 1
+			: 0);
+	}
+}
+
+#endif //_WIN32
