47f107bf74bf84775deaa18866843e70d5b3d32d
greeenlaser <sanderveski@gmail.com>
2026-06-23 14:36:02 +0300

    setting up a different structure

--- src/graphics/models/kg_model.cpp
+++ src/graphics/models/kg_model.cpp
@@ -7,23 +7,11 @@
 
 #include "graphics/models/kg_model.hpp"
 #include "core/kg_core.hpp"
-#include "core/kg_context.hpp"
-#include "graphics/models/kg_model_primitive.hpp"
 
-using KalaGraphics::Core::KalaGraphicsCore;
-using KalaGraphics::Core::GraphicsContext;
-using KalaGraphics::Core::GraphicsContextData;
 using KalaGraphics::Core::MAX_NAME_LENGTH;
 
 using KalaHeaders::KalaLog::Log;
 using KalaHeaders::KalaLog::LogType;
-
-using std::to_string;
-
-static bool ContextExists(u32 contextID)
-{
-    return GraphicsContext::GetRegistry().createdContent.contains(contextID);
-} 
 
 namespace KalaGraphics::Graphics
 {
@@ -65,35 +53,4 @@ namespace KalaGraphics::Graphics
     u32 Model::GetGraphicsContextID() const { return contextID; }
     u32 Model::GetVulkanContextID() const { return vulkanID; }
     u32 Model::GetShaderID() const { return shaderID; }
-
-    void Model::SetColor(const vec3& newColor)
-    {
-        if (newColor.x < 0.0f
-            || newColor.y < 0.0f
-            || newColor.z < 0.0f)
-        {
-            Log::Print(
-                "Failed to set new model color because one of its values was too low! It must be 0.0f to 1.0f.",
-                "KG_MODEL",
-                LogType::LOG_ERROR,
-                2);
-
-            return;
-        }
-        if (newColor.x > 1.0f
-            || newColor.y > 1.0f
-            || newColor.z > 1.0f)
-        {
-            Log::Print(
-                "Failed to set new model color because one of its values was too high! It must be 0.0f to 1.0f.",
-                "KG_MODEL",
-                LogType::LOG_ERROR,
-                2);
-
-            return;
-        }
-
-        color = newColor;
-    }
-    const vec3& Model::GetColor() const { return color; }
 }
