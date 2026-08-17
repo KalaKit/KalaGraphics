de6b06ee1eebbe43f5d0479de1de4c787de9764c
greeenlaser <sanderveski@gmail.com>
2026-08-17 21:47:43 +0300

    fixed msvc export

--- mf_ex.sh
+++ mf_ex.sh
@@ -27,7 +27,7 @@ case "$1" in
         OUT_NAME=KalaGraphics-Windows
 
         LIB_NAME=kalagraphics
-        LIB_EXT=
+        LIB_EXT=lib
         LIB_ORIGIN=build/release-windows
         ;;
     *)
